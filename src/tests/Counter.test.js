e0cca7d5a1ab11136618b492ad0c754f40ca8e8a
jsulkes20 <95899154+jsulkes20@users.noreply.github.com>
2023-04-08 21:48:54 -0400

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -2,40 +2,33 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Counter from "../components/Counter";
 
-describe("Counter component", () => {
+describe("Counter Component", () => {
   beforeEach(() => {
     render(<Counter />);
   });
 
   test("renders counter message", () => {
-    const message = document.querySelector("h2");
-    expect(message).toHaveTextContent("Counter");
+    const messageElement = document.querySelector("h2");
+    expect(messageElement).toBeInTheDocument();
+    expect(messageElement.textContent).toBe("Counter");
   });
 
   test("should render initial count with value of 0", () => {
-    const count = document.querySelector("[data-testid='count']");
-    expect(count).toHaveTextContent(0);
+    const countElement = document.querySelector('[data-testid="count"]');
+    expect(countElement.textContent).toBe("0");
   });
 
   test("clicking + increments the count", () => {
-    const count = document.querySelector("[data-testid='count']");
-    const incrementBtn = document.querySelector("button:first-of-type");
-
-    fireEvent.click(incrementBtn);
-    expect(count).toHaveTextContent(1);
-
-    fireEvent.click(incrementBtn);
-    expect(count).toHaveTextContent(2);
+    const countElement = document.querySelector('[data-testid="count"]');
+    const incrementButton = document.querySelector('button:nth-of-type(1)');
+    fireEvent.click(incrementButton);
+    expect(countElement.textContent).toBe("1");
   });
 
   test("clicking - decrements the count", () => {
-    const count = document.querySelector("[data-testid='count']");
-    const decrementBtn = document.querySelector("button:last-of-type");
-
-    fireEvent.click(decrementBtn);
-    expect(count).toHaveTextContent(-1);
-
-    fireEvent.click(decrementBtn);
-    expect(count).toHaveTextContent(-2);
+    const countElement = document.querySelector('[data-testid="count"]');
+    const decrementButton = document.querySelector('button:nth-of-type(2)');
+    fireEvent.click(decrementButton);
+    expect(countElement.textContent).toBe("-1");
   });
 });
